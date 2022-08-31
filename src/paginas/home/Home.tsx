1e5ec11a3beb99bcfab4050e6a0ddcf5e9631c1a
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-08-30 22:44:58 -0300

    @Reynald-Albuquerque-Costa[Task:Inicializando o React]{30/08/2022 22:44}

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://acsp.com.br/public/upload/gallery/oficios/acsp_post_09_07_site.png" alt="Imagem tela inicial"
+             className="img" />
+        </>
+    );
+}
+
+export default Home;
