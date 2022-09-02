49057c54f67f15c16daf4c1c034588c261dbf1c9
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-09-02 13:05:01 -0300

    @Reynald-Albuquerque-Costa[Task: ADD componente login]{02/09/2022 13:04}

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home'
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Home />
+            <div style={{ minHeight: '100vh' }}>
+              <Routes>
+              <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+              </Routes>  
+            </div>
       <Footer />
-    </>
-
-  )
+    </Router>
+  );
 }
 
 export default App;
