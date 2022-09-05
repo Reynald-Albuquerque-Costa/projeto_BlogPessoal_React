3766ced1c033eff71c543f80de49d49bbd75182d
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-09-05 10:38:05 -0300

    @Reynald-Albuquerque-Costa[Task:ADD comunicação com o bac-kend componente login]{05/09/2022 10:37}

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { api } from '../../services/Services';
 import { Box } from '@mui/material';
+import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
 
 function Login(){
+
+    let history = useNavigate();
+    const[token, setToken] = useLocalStorage('token');
+
+    const[userLogin, setUserLogin]= useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+    )
+
+    function updateModel(e: ChangeEvent<HTMLInputElement>){
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
+
+        useEffect(()=>{
+            if(token != ''){
+                history('/home')
+            }
+        },[token])
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+        e.preventDefault();
+        try{
+            const resposta = await api.post(`/usuarios/logar`, userLogin)
+            setToken(resposta.data.token)
+
+            alert('Usuário logado com sucesso')
+        }
+        catch(error){
+            alert('Dados do usuário incorreto. Erro ao logar')
+        }
+
+    }
+
     return(
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary'>
                                     Logar
                                 </Button>
-                            </Link>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
